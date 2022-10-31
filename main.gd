933a6fec5105ecc66221466db96f71966393cbfa
heppocogne <83043568+heppocogne@users.noreply.github.com>
2022-10-31 19:49:37 +0900

    Refactor rest_button Add `get_node_property()` Add `set_node_property()`

--- main.gd
+++ main.gd
@@ -9,3 +9,15 @@ const export_parameters:=PoolStringArray([
 	"BackgroundPanel/HSplitContainer/PanelContainer/MarginContainer/VBoxContainer/Symmetry/Diagonal:pressed",
 	"BackgroundPanel/HSplitContainer/PanelContainer/MarginContainer/VBoxContainer/VBoxContainer/ComplexityEdit/SpinBox:value"
 ])
+
+
+func get_node_property(path:String):
+	var colon_index:int=path.find(":")
+	var node:Node=get_node(path.left(colon_index))
+	return node.get_indexed(path.right(colon_index))
+
+
+func set_node_property(path:String, val):
+	var colon_index:int=path.find(":")
+	var node:Node=get_node(path.left(colon_index))
+	node.set_indexed(path.right(colon_index), val)
