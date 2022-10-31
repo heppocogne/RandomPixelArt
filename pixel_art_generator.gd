c95e541d138df1850bd53435f2a20478b5693c48
heppocogne <83043568+heppocogne@users.noreply.github.com>
2022-10-31 19:37:48 +0900

    Update pixel_art_generator

--- pixel_art_generator.gd
+++ pixel_art_generator.gd
@@ -2,6 +2,12 @@ class_name PixelArtGenerator
 extends Reference
 
 
-func generate(size:Vector2)->Image:
+func generate(width:int,height:int)->Image:
 	var image:=Image.new()
+	image.create(width,height,false,Image.FORMAT_BPTC_RGBA)
+	image.lock()
+	for y in height:
+		for x in width:
+			image.set_pixel(x,y,Color(randf(),randf(),randf(),randf()))
+	image.unlock()
 	return image
