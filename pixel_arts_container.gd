c3c8cd098469d954e4cea8f60c6df7a46343a3d2
heppocogne <83043568+heppocogne@users.noreply.github.com>
2022-10-31 18:08:24 +0900

    Update columns on WidthEdit's value changed

--- pixel_arts_container.gd
+++ pixel_arts_container.gd
@@ -1,2 +1,13 @@
 extends GridContainer
 
+var h_separation:int=get_constant("hseparation")
+#var v_separation:int=get_constant("vseparation")
+onready var width_edit:SpinBox=$"../../PanelContainer/MarginContainer/VBoxContainer/Size/Edit/WidthEdit"
+
+
+func _update_columns():
+	_on_WidthEdit_value_changed(width_edit.value)
+
+
+func _on_WidthEdit_value_changed(width:int):
+	columns=int((rect_size.x+h_separation)/(width+h_separation))
