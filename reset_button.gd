95f41b08b143d2f2ef970896675d486c1c598bfc
heppocogne <83043568+heppocogne@users.noreply.github.com>
2022-10-31 17:39:03 +0900

    Update node structure Add radio button

--- reset_button.gd
+++ reset_button.gd
@@ -1,19 +1,22 @@
 extends Button
 
-export(Array,String) var target_pathes:Array
+const target_pathes:=PoolStringArray([
+	"../Size/Edit/WidthEdit:value",
+	"../Size/Edit/HeightEdit:value",
+	"../Symmetry/None:pressed",
+	"../VBoxContainer/ComplexityEdit/SpinBox:value"
+])
 # [object,property_path,value]
 var initial_values:Array
 
 func _init():
-	target_pathes=[]
 	initial_values=[]
 
 
 func _on_Main_ready():
-	var main:Control=get_tree().root.get_node("Main")
 	for path in target_pathes:
 		var colon_index:int=path.find(":")
-		var node:Node=main.get_node(path.left(colon_index))
+		var node:Node=get_node(path.left(colon_index))
 		var property:NodePath=path.right(colon_index)
 		initial_values.push_back([node,property,node.get_indexed(property)])
 
