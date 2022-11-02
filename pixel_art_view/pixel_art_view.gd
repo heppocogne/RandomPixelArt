df57e12f7f379453374cc86c3698e5def5a3e8e8
heppocogne <83043568+heppocogne@users.noreply.github.com>
2022-11-02 14:46:41 +0900

    Support save by right-click

--- pixel_art_view/pixel_art_view.gd
+++ pixel_art_view/pixel_art_view.gd
@@ -1,10 +1,29 @@
 class_name PixelArtView
 extends TextureRect
 
-# TODO: support save by right-click
+# TODO: add selection indicator
 
+var context_menu:PopupMenu
 var noises:Array
 
 
 func _init():
 	noises=[]
+
+
+func _gui_input(event:InputEvent):
+	if event is InputEventMouseButton:
+		var mb:=event as InputEventMouseButton
+		if mb.button_index==BUTTON_RIGHT and mb.pressed:
+			$FileDialog.popup_centered(Vector2(600,400))
+
+
+func _on_FileDialog_file_selected(path:String):
+	var img:Image=texture.get_data()
+	var save_error:int
+	if path.ends_with(".png"):
+		save_error=img.save_png(path)
+	elif path.ends_with(".exr"):
+		save_error=img.save_exr(path)
+	elif path.ends_with(".tres") or path.ends_with(".res"):
+		save_error=ResourceSaver.save(path,img)
