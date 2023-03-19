aae43867bb681afa474c7e7aa0ef8619f634680e
heppocogne <83043568+heppocogne@users.noreply.github.com>
2023-03-19 10:43:45 +0900

    Update export script

--- project_export.ps1
+++ project_export.ps1
@@ -10,22 +10,22 @@ New-Item -ItemType File -Force -Path "./project_export/.gdignore"
 
 # Windows export
 New-Item -ItemType Directory -Force -Path "./project_export/windows"
-Start-Process -FilePath $godot_path -ArgumentList "--verbose --export `"Windows Desktop`" $("project_export/windows/"+$gameName+".exe")" -Wait
+Start-Process -FilePath $godot_path -ArgumentList "--verbose --export-release `"Windows Desktop`" $("project_export/windows/"+$gameName+".exe")" -Wait
 Set-Location "./project_export/windows"
 Start-Process -FilePath $7z_path -ArgumentList "a -mx=9 $("../"+$gameName+"_windows.zip") ./*" -NoNewWindow -Wait
 Set-Location "../../"
 
 # Linux Export
 New-Item -ItemType Directory -Force -Path "project_export/linux"
-Start-Process -FilePath $godot_path -ArgumentList "--verbose --export `"Linux/X11`" $("project_export/linux/"+$gameName+".x86_64")" -Wait
+Start-Process -FilePath $godot_path -ArgumentList "--verbose --export-release `"Linux/X11`" $("project_export/linux/"+$gameName+".x86_64")" -Wait
 Set-Location "project_export/linux"
 Start-Process -FilePath $7z_path -ArgumentList "a -mx=9 $("../"+$gameName+"_linux.zip") ./*" -NoNewWindow -Wait
 Set-Location "../../"
 
 # HTML5 Export
-New-Item -ItemType Directory -Force -Path "./project_export/html5"
-Start-Process -FilePath $godot_path -ArgumentList "--verbose --export `"HTML5`" $("project_export/html5/"+$gameName+".html")" -Wait
-Set-Location "./project_export/html5"
+New-Item -ItemType Directory -Force -Path "./project_export/web"
+Start-Process -FilePath $godot_path -ArgumentList "--verbose --export-release `"Web`" $("project_export/web/"+$gameName+".html")" -Wait
+Set-Location "./project_export/web"
 Rename-Item -Path ($gameName + ".html") -NewName "index.html"
-Start-Process -FilePath $7z_path -ArgumentList "a -mx=9 $("../"+$gameName+"_html5.zip") ./*" -NoNewWindow -Wait
+Start-Process -FilePath $7z_path -ArgumentList "a -mx=9 $("../"+$gameName+"_web.zip") ./*" -NoNewWindow -Wait
 Set-Location "../../"
