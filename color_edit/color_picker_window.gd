34be701a118c3fcb4b70d3a9a15864f3dd48817e
heppocogne <83043568+heppocogne@users.noreply.github.com>
2023-03-21 10:10:45 +0900

    Modify color picker's behavior

--- color_edit/color_picker_window.gd
+++ color_edit/color_picker_window.gd
@@ -3,12 +3,16 @@ extends Window
 const size_ext:=Vector2(16,16)
 
 func _ready():
-	$ColorPicker.position=Vector2(8,8)
+	$ScrollContainer/ColorPicker.position=Vector2(8,8)
 
 
 func _on_ColorPicker_resized():
-	min_size=$ColorPicker.size+size_ext
+	min_size=$ScrollContainer/ColorPicker.size+size_ext
 
 
 func _on_close_requested():
+	hide()
+
+
+func _on_focus_exited():
 	hide()
--- color_edit/color_rect.gd
+++ color_edit/color_rect.gd
@@ -5,9 +5,9 @@ func _gui_input(event:InputEvent):
 	if event is InputEventMouseButton:
 		var mb:=event as InputEventMouseButton
 		if mb.pressed:
-			$Window/ColorPicker.color=color
-			$Window.popup_centered($Window/ColorPicker.size+$Window.size_ext)
+			$Window/ScrollContainer/ColorPicker.color=color
+			$Window.popup_centered($Window/ScrollContainer/ColorPicker.size+$Window.size_ext)
 
 
-func _on_window_close_requested():
-	color=$Window/ColorPicker.color
+func _on_color_picker_color_changed(color:Color):
+	self.color=color
